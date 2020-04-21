d52247ca1410ed18deaf53f948012d92efa036a8
danielbengtsen <cph-db145@cphbusiness.dk>
2020-04-21 13:28:05 +0200

    Initialisering af lister efter submit er fikset.

--- src/main/java/PresentationLayer/FladtTag.java
+++ src/main/java/PresentationLayer/FladtTag.java
@@ -1,5 +1,6 @@
 package PresentationLayer;
 
+import FunctionLayer.Initialisation;
 import FunctionLayer.LogicFacade;
 import FunctionLayer.LoginSampleException;
 import FunctionLayer.Validation;
@@ -16,7 +17,6 @@ public class FladtTag extends Command {
         HttpSession session = request.getSession();
         String tmpCarportLength = request.getParameter("length");
         String tmpCarportWidth = request.getParameter("width");
-        System.out.println(tmpCarportLength + "    " + tmpCarportWidth);
         String tmpShedLength = "";
         int shedLength = 0;
         String tmpShedWidth = "";
@@ -25,18 +25,24 @@ public class FladtTag extends Command {
         Boolean hasShed = false;
         int price = 0;
 
-        /*if(request.getParameter("checkboxShed").equals("on")) {
+        if(request.getParameter("checkboxShed").equals("on")) {
             tmpShedLength = request.getParameter("shedLength");
             tmpShedWidth = request.getParameter("shedWidth");
             shedLength = Validation.getInteger(tmpShedLength);
             shedWidth = Validation.getInteger(tmpShedWidth);
             hasShed = true;
-        }*/
+        }
 
         int carportLength = Validation.getInteger(tmpCarportLength);
         int carportWidth = Validation.getInteger(tmpCarportWidth);
 
         LogicFacade.addFlatCarportToCustOrder(carportLength, carportWidth, hasShed, shedWidth, shedLength, roofMaterial, price);
+
+        request.setAttribute("carport_lengths", Initialisation.getCarportLengths());
+        request.setAttribute("carport_widths", Initialisation.getCarportWidths());
+        request.setAttribute("carport_roofs", Initialisation.getRoofs());
+        request.setAttribute("shed_lengths", Initialisation.getShedLengths());
+        request.setAttribute("shed_widths", Initialisation.getShedWidths());
 
         return "fladttag";
     }
